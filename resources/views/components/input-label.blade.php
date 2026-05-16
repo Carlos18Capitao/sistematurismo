982bbf3507b1a563d07f852dfe0da5ea0fe16957
Carlos Capitao <carlos18mauro@gmail.com>
2026-05-16 21:28:45 +0100

    apagar ficheiro desnecessario blade.

--- resources/views/components/input-label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block font-semibold text-sm text-gray-700 mb-1']) }}>
-    {{ $value ?? $slot }}
-</label>
--- resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'w-full border border-gray-300 focus:border-orange-500 focus:ring-orange-500 rounded-xl shadow-sm px-4 py-2.5 text-gray-900 placeholder-gray-400 transition']) }}>
